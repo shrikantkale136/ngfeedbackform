a8bfa559ee450e1760d6abcefdeaf94818680461
shrikantkale136 <shrikantkale11@yahoo.com>
2020-04-11 20:49:02 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   name = 'Feeback Form';
   feedbackArray = [
-    {name: 'Prop1', value: 0 },
-    {name: 'Prop2', value: 0 },
-    {name: 'Prop3', value: 0 },
-    {name: 'Prop4', value: 0 },
-    {name: 'Prop5', value: 0 }
+    {name: 'Prop1', value: 1 },
+    {name: 'Prop2', value: 1 },
+    {name: 'Prop3', value: 1 },
+    {name: 'Prop4', value: 1 },
+    {name: 'Prop5', value: 1 }
   ];
   element: HTMLElement;
-  images = [
-    {id : 1, img: "https://cdn.pixabay.com/photo/2017/03/05/21/55/emogi-1799208_960_720.png"},
-    {id : 2, img: "https://cdn.pixabay.com/photo/2016/11/04/23/16/emoticon-2120024_960_720.png"}]
-
-public img ;
-
   getVal(name,value){
     console.log(name,value)
     this.element = document.getElementById(name) as HTMLElement;
