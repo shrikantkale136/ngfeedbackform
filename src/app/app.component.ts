91f544fa99fc037ccde25090c489e9b800799283
shrikantkale136 <shrikantkale11@yahoo.com>
2020-04-11 12:42:36 +0530

    2 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,25 @@ export class AppComponent  {
     {name: 'Prop4', value: 0 },
     {name: 'Prop5', value: 0 }
   ];
+  element: HTMLElement;
+  images = [
+    {id : 1, img: "https://cdn.pixabay.com/photo/2017/03/05/21/55/emoticon-2120024_960_720.png"},
+    {id : 2, img: "https://cdn.pixabay.com/photo/2016/11/04/23/16/emogi-1799208_960_720.png"}]
+
+public img ;
+
+  getVal(name,value){
+    console.log(name,value)
+    this.element = document.getElementById(name) as HTMLElement;
+    console.log(this.element)
+    if(value === 1){
+      this.element.setAttribute("src","https://cdn.pixabay.com/photo/2017/03/05/21/55/emoticon-2120024_960_720.png");
+    }else {
+      this.element.setAttribute("src","https://cdn.pixabay.com/photo/2016/11/04/23/16/emogi-1799208_960_720.png");
+    }
+  }
+
+
 
   submit(f){
     console.log(f.value)
